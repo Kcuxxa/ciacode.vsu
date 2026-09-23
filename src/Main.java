c480742ed846e85d2049f1a4fc1ad6ea8019fa70
Kcuxxa <mkd2020@bk.ru>
2026-09-23 11:38:24 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static int n;
+    static boolean[] flag;
+    static int m;
+
+    static void generate(int count, double p) {
+        n=count;
+        flag=new boolean[n];
+        for(int i=0;i<n;i++){
+            flag[i]=Math.random()<p;
+        }
+    }
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
